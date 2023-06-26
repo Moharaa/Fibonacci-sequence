ecd901d743ee233c91b8a5a3cfc5f68264f01eb8
Moharaa <137526332+Moharaa@users.noreply.github.com>
2023-06-26 19:46:52 +0330

    Create Fopo.c

---
+++ Fopo.c
@@ -0,0 +1,21 @@
+<include <iostream#
+
+;using namespace std
+
+    }()int main
+    
+;int a,b,c,i,n
+    
+;a=1
+;b=1
+;cin>>n
+    
+    }   for(i=3;i<=n;i++)
+;c=a+b
+;a=b
+;b=c
+ { 
+    
+; cout<<n<<"omin jomleye fibonacci: "<<c
+
+  {
